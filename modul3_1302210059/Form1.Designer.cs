840776741f276c283e0f716153e221c7f49f59f0
fadelalif <126957890+fadelalif@users.noreply.github.com>
2023-03-08 17:00:22 +0700

    Menambahkan Implementasi Kalkulator

--- modul3_1302210059/Form1.Designer.cs
+++ modul3_1302210059/Form1.Designer.cs
@@ -136,11 +136,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(110, 49);
+            this.label1.Location = new System.Drawing.Point(127, 49);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(0, 13);
             this.label1.TabIndex = 9;
-            this.label1.Text = "label1";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // button10
--- modul3_1302210059/Form1.cs
+++ modul3_1302210059/Form1.cs
@@ -12,6 +12,7 @@ namespace modul3_1302210059
 {
     public partial class Form1 : Form
     {
+        private string simpan;
         public Form1()
         {
             InitializeComponent();
@@ -24,67 +25,69 @@ namespace modul3_1302210059
 
         private void label1_Click(object sender, EventArgs e)
         {
-
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            simpan = label1.Text;
+            this.label1.Text = "";
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            var hasil = int.Parse(simpan) + int.Parse(label1.Text);
+            label1.Text = ""+ hasil;
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-
+            this.label1.Text += "0";
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-
+            this.label1.Text += "1";
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-
+            this.label1.Text += "2";
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-
+            this.label1.Text += "3";
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
-
+            this.label1.Text += "4";
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
-
+            this.label1.Text += "5";
         }
 
         private void button9_Click(object sender, EventArgs e)
         {
-
+            this.label1.Text += "6";
         }
 
         private void button10_Click(object sender, EventArgs e)
         {
-
+            this.label1.Text += "7";
         }
 
         private void button11_Click(object sender, EventArgs e)
         {
-
+            this.label1.Text += "8";
         }
 
         private void button12_Click(object sender, EventArgs e)
         {
-
+            this.label1.Text += "9";
         }
     }
 }
